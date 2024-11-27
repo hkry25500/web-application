c466197f5ddfd038fd50693731802867d79d0f70
Yukikaze924 <yukikaze924@gmail.com>
2024-11-28 03:34:16 +0800

    fix: Inappropriate display props at movie page

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -16,55 +16,53 @@ export default async function MoviePage({ movie }: any)
 
     return (
         <>
-            <div id='movie-page' className="flex flex-col w-full min-h-screen">
-                {/* Main */}
-                <main className="flex-1 justify-center z-0 py-5 px-5">
+            {/* Main */}
+            <main className="w-full min-h-100-70 py-5 px-5 z-0">
 
-                    <div className="flex flex-wrap max-w-screen-2xl mx-auto 2xl:mt-10 2xl:gap-10">
-                        {/* main col */}
-                        <div className="w-full md:flex-1">
-                            {/* player */}
-                            <div className="bg-black relative mb-3" style={{ paddingTop: "55%" }}>
-                                <Player movie={movie} />
-                            </div>
-                            {/* video info */}
-                            <div className="flex flex-wrap items-end pt-2">
-                                {/* title */}
-                                <div className="mb-4">
-                                    <h1 className="text-xl text-[var(--text-color-primary)] font-bold tracking-tighter shrink-0 my-2">{ movie.title }</h1>
-                                    <span className='text-sm text-[var(--text-color-secondary)]'>{ movie.description }</span>
-                                </div>
-                                {
-                                    movie.interests.map((interest: string) => {
-                                        return (
-                                            <div style={{ paddingTop: "0.2em", paddingBottom: "0.2rem" }}
-                                                 className="text-xs mr-2 px-3 bg-gray-200 text-gray-800 rounded-full"
-                                                 key={interest}>
-                                                { interest }
-                                            </div>
-                                        )
-                                    })
-                                }
-                                <hr className="w-full h-px bg-[var(--border-color-secondary)] border-0 m-0 my-3 " />
-                            </div>
+                <div className="flex flex-wrap max-w-screen-2xl mx-auto 2xl:mt-10 2xl:gap-10">
+                    {/* main col */}
+                    <div className="w-full md:flex-1">
+                        {/* player */}
+                        <div className="bg-black relative mb-3" style={{ paddingTop: "55%" }}>
+                            <Player movie={movie} />
                         </div>
-
-                        {/* aside */}
-                        <aside className="w-full max-md:hidden md:max-w-xs xl:max-w-full xl:w-1/4 md:pl-5 mt-5 md:mt-0">
-                            <ChatBox room={movie.id} />
-                        </aside>
-                    </div>
-
-                    <div className='flex flex-wrap max-w-screen-2xl mx-auto 2xl:mt-4 2xl:gap-10'>
-                        <CommentDetail movie={movie} />
-                        <div className='w-full max-md:hidden md:max-w-xs xl:max-w-full xl:w-1/4 md:pl-5 mt-5 md:mt-0'>
-                            {/* Holding this place for future impl (probably ads or recommendation list...) */}
-                            <div className='placeholder w-full h-full border-1'></div>
+                        {/* video info */}
+                        <div className="flex flex-wrap items-end pt-2">
+                            {/* title */}
+                            <div className="mb-4">
+                                <h1 className="text-xl text-[var(--text-color-primary)] font-bold tracking-tighter shrink-0 my-2">{ movie.title }</h1>
+                                <span className='text-sm text-[var(--text-color-secondary)]'>{ movie.description }</span>
+                            </div>
+                            {
+                                movie.interests.map((interest: string) => {
+                                    return (
+                                        <div style={{ paddingTop: "0.2em", paddingBottom: "0.2rem" }}
+                                                className="text-xs mr-2 px-3 bg-gray-200 text-gray-800 rounded-full"
+                                                key={interest}>
+                                            { interest }
+                                        </div>
+                                    )
+                                })
+                            }
+                            <hr className="w-full h-px bg-[var(--border-color-secondary)] border-0 m-0 my-3 " />
                         </div>
                     </div>
 
-                </main>
-            </div>
+                    {/* aside */}
+                    <aside className="w-full max-md:hidden md:max-w-xs xl:max-w-full xl:w-1/4 md:pl-5 mt-5 md:mt-0">
+                        <ChatBox room={movie.id} />
+                    </aside>
+                </div>
+
+                <div className='flex flex-wrap max-w-screen-2xl mx-auto 2xl:mt-4 2xl:gap-10'>
+                    <CommentDetail movie={movie} />
+                    <div className='w-full max-md:hidden md:max-w-xs xl:max-w-full xl:w-1/4 md:pl-5 mt-5 md:mt-0'>
+                        {/* Holding this place for future impl (probably ads or recommendation list...) */}
+                        <div className='placeholder w-full h-full border-1'></div>
+                    </div>
+                </div>
+
+            </main>
         </>
     )
 }
